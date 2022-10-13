a7e23d5bed2ca51c812224e7c389224c5f11a792
Greta Lileikyte <g.lileikyte@hotmail.co.uk>
2022-10-13 16:44:17 +0100

    Commenting Code Completed

--- java-oop/classwork/PracticeProgramming/src/main/java/com/sg/foundations/basics/CommentingCode.java
+++ java-oop/classwork/PracticeProgramming/src/main/java/com/sg/foundations/basics/CommentingCode.java
@@ -10,5 +10,16 @@ package com.sg.foundations.basics;
  * @author Greta
  */
 public class CommentingCode {
-    
+     public static void main(String[] args) {
+
+        System.out.println("/* A multiple line comments */");
+        System.out.println("// Single line comments");// do not execute!
+
+        // Comments can be on their own line
+        System.out.println("/** comments a doc */"); // or they can share like this
+
+        // However if you put the // BEFORE a line of code
+        // System.out.println("Then it is considered a comment");
+        // System.out.println("and it won't execute!");
+    }
 }
