ca5024c26e735b82036add7a3352cda03f1f244a
Greta Lileikyte <g.lileikyte@hotmail.co.uk>
2022-10-16 16:31:27 +0100

    Guess me Completed

--- java-oop/classwork/PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/ifs/GuessMe.java
+++ java-oop/classwork/PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/ifs/GuessMe.java
@@ -1,23 +1,30 @@
-/**
- * @author Greta
- * email: g.lileikyte@hotmail.co.uk
- * date: 11/10/2022
- * purpose: My first Hello World program in an IDE
- */
+//@author Greta
 package com.sg.foundations.flowcontrol.ifs;
 
 import java.util.Scanner;
 
 public class GuessMe {
-      public static void main(String[] args) {
-       
-       int userPick;
-       Scanner myScanner = new Scanner(System.in);
-       
-       System.out.println("Pick a number:");
-       userPick = myScanner.nextLine();
-      
-      System.out.println("Window height = " + stringHeight);
-    
-         }
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        int myPick = 11;
+        int userPick;
+
+        System.out.println("I've chosen a number. Betcha can't guess it!");
+
+        System.out.println("Your guess: ");
+        String userInputS = userInput.nextLine();
+        userPick = Integer.parseInt(userInputS);
+
+        if (userPick == myPick) {
+            System.out.println("Wow, nice guess! That was it!");
+        }
+
+        if (userPick < myPick) {
+            System.out.println("Ha, nice try - too low! I chose " + myPick + "!");
+        }
+        if (userPick > myPick) {
+            System.out.println("Too bad, way too high. I chose " + myPick + ".");
+        }
+    }
 }
