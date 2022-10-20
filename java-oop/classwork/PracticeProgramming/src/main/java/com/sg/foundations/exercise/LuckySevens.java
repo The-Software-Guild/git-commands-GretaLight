ebed6d73647efeaaf4d780cda9e09295873a1d49
Greta Lileikyte <g.lileikyte@hotmail.co.uk>
2022-10-20 23:02:58 +0100

    Lucky Sevens Completed

--- java-oop/classwork/PracticeProgramming/src/main/java/com/sg/foundations/exercise/LuckySevens.java
+++ java-oop/classwork/PracticeProgramming/src/main/java/com/sg/foundations/exercise/LuckySevens.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.sg.foundations.exercise;
 
 import java.util.Random;
@@ -18,13 +13,10 @@ public class LuckySevens {
 
         Scanner input = new Scanner(System.in);
         Random dice1Roll = new Random();
-        Random dice2Roll = new Random();
 
         int min = 1;
         int max = 6;
         int range = max - min;
-        int dice1Range = dice1Roll.nextInt(range) + min;
-        int dice2Range = dice2Roll.nextInt(range) + min;
 
         int dice1Random;
         int dice2Random;
@@ -33,29 +25,29 @@ public class LuckySevens {
         int totalRolls = 1;
         int maxRolls = 0;
         int currentGame = 1;
-        int maxGame= 1;
+        int maxGame = 1;
         String userInput;
 
         System.out.println("How many dollars do you have?");
         userInput = input.nextLine();
         dollars = Integer.parseInt(userInput);
-        int maximum = dollars;;
+        int maximum = dollars;
 
         while (dollars > 0) {
-            dice1Random = dice1Range;
-            dice2Random = dice2Range;
+            dice1Random = dice1Roll.nextInt(range) + min;
+            dice2Random = dice1Roll.nextInt(range) + min;
             result = dice1Random + dice2Random;
-
 
             if (result == 7) {
                 dollars = dollars + 4;
                 if (dollars > maximum) {
                     maximum = dollars;
                     maxGame = currentGame;
-            }} else {
+                }
+            } else {
                 dollars = dollars - 1;
             }
-            currentGame ++;
+            currentGame++;
             //  if (newDollar > dollars && maximum < newDollar) {
             //     maximum = newDollar;
             //     maxRolls = totalRolls;
